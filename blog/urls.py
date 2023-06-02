8b2b1e3d29f25244f0a2f13629c29719b74b01df
anikronjon <anikronjon.swe@gmail.com>
2023-06-02 22:01:20 +0600

    update unnecessary

--- blog/urls.py
+++ blog/urls.py
@@ -6,7 +6,7 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.post_list_detail, name='post_list'),
     path('<slug:slug>/', views.post_list_detail, name='post_detail'),
-    path('<slug:slug>/like/', views.like_post, name='like_post'),
-    path('<slug:slug>/comment/', views.comment_post, name='comment_post'),
-    path('<slug:slug>/rate/', views.rating_view, name='rate_post'),
+    path('<slug:slug>/like/', views.like_post, name='post_like'),
+    path('<slug:slug>/comment/', views.comment_post, name='post_comment'),
+    path('<slug:slug>/rate/', views.rating_view, name='post_rate'),
 ]
