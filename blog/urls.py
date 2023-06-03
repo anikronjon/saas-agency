2f4a97bbdb8e06d78aba1d4b7f80c3cbc8ec4059
anikronjon <anikronjon.swe@gmail.com>
2023-06-03 17:48:11 +0600

    update blog comment

--- blog/urls.py
+++ blog/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 app_name = 'blog'
 urlpatterns = [
-    path('', views.post_list_detail, name='post_list'),
-    path('<slug:slug>/', views.post_list_detail, name='post_detail'),
-    path('<slug:slug>/like/', views.like_post, name='post_like'),
-    path('<slug:slug>/comment/', views.comment_post, name='post_comment'),
-    path('<slug:slug>/rate/', views.rating_view, name='post_rate'),
+    path('', views.post_list, name='post_list'),
+    path('<slug:slug>/', views.PostDetailView.as_view(), name='post_detail'),
+    # path('<slug:slug>/like/', views.like_post, name='post_like'),
+    # path('<slug:slug>/comment/', views.comment_post, name='post_comment'),
+    # path('<slug:slug>/rate/', views.rating_post, name='post_rate'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,54 +1,46 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.contenttypes.models import ContentType
+from django.utils.decorators import method_decorator
+from django.views.decorators.http import require_POST
 from django.shortcuts import render, redirect, get_object_or_404
+from django.views import View
 from .models import Category, Tag, Post, Like, Comment, Rating
+from .forms import CommentForm, RatingForm
 
 
-def post_list_detail(request, slug=None):
-    if slug:
+def post_list(request):
+    posts = Post.objects.filter(published='pub').order_by('-created_at')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+class PostDetailView(View):
+    def get(self, request, slug=None):
         post = get_object_or_404(Post, slug=slug, published='pub')
         likes = post.likes.all()
-        comments = post.comments.all()
         rating = post.ratings.all()
-        return render(request, 'blog/post_detail.html', {'post': post, 'likes': likes, 'comments': comments, 'rating': rating})
-    else:
-        posts = Post.objects.filter(published='pub').order_by('-created_at')
-        return render(request, 'blog/post_list.html', {'posts': posts})
+        comments = post.comments.all()
+        form = CommentForm()
 
+        return render(request, 'blog/post_detail.html',
+                      {'post': post, 'likes': likes, 'rating': rating, 'comments': comments, 'form': form})
 
-@login_required
-def like_post(request, slug):
-    post = get_object_or_404(Post, slug=slug)
-    if request.method == 'POST':
-        # Check if the user has already liked the post
-        liked = Like.objects.filter(user=request.user, content_object=post).exists()
-        if liked:
-            pass
-        else:
-            # Create a new Like object
-            like = Like(user=request.user, content_object=post)
-            like.save()
+    @method_decorator(login_required(login_url='/account/signin/'))
+    def post(self, request, slug=None):
+        post = get_object_or_404(Post, slug=slug)
+        form = CommentForm(request.POST)
 
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.user = request.user
+            comment.content_object = post
+            comment.save()
+            return redirect('blog:post_detail', slug=post.slug)
 
-@login_required
-def comment_post(request, slug):
-    post = get_object_or_404(Post, slug=slug)
+        post = get_object_or_404(Post, slug=slug, published='pub')
+        likes = post.likes.all()
+        rating = post.ratings.all()
+        comments = post.comments.all()
 
-    if request.method == 'POST':
-        comment_content = request.POST.get('comment_content')
-        comment = Comment(user=request.user, content_object=post, content=comment_content)
-        comment.save()
+        return render(request, 'blog/post_detail.html',
+                      {'post': post, 'likes': likes, 'rating': rating, 'comments': comments, 'form': form})
 
-
-@login_required
-def rating_view(request, slug):
-    post = get_object_or_404(Post, slug=slug)
-
-    if request.method == 'POST':
-        rating_value = request.POST.get('rating_value')
-        if rating_value:
-            rated = Rating.objects.filter(user=request.user, content_object=post).exists()
-            if rated:
-                pass
-            else:
-                rating = Rating(user=request.user, content_object=post, value=rating_value)
-                rating.save()
