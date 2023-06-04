272a22d6021a88c600c147487e80f7ab24e56bb4
anikronjon <anikronjon.swe@gmail.com>
2023-06-04 22:52:15 +0600

    updte common export: models remove

--- common/__init__.py
+++ common/__init__.py
@@ -1,8 +1,6 @@
 from .validator import image_size_validation, video_size_validation
-from .models import Like, Comment, Rating
 
 
 __all__ = [
-    'Like', 'Comment', 'Rating',
     'image_size_validation', 'video_size_validation',
 ]
