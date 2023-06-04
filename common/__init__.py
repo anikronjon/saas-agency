312b879a7d059b18529c9174b0c116cc468c3a56
anikronjon <anikronjon.swe@gmail.com>
2023-06-04 22:46:50 +0600

    utility > Common, add shared model

--- common/__init__.py
+++ common/__init__.py
@@ -1,6 +1,8 @@
 from .validator import image_size_validation, video_size_validation
+from .models import Like, Comment, Rating
 
 
 __all__ = [
+    'Like', 'Comment', 'Rating',
     'image_size_validation', 'video_size_validation',
 ]
