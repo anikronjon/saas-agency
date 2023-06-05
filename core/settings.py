f0e363d456bc532a8132275fec6f980ecac82008
anikronjon <anikronjon.swe@gmail.com>
2023-06-06 05:33:56 +0600

    update common

--- core/settings.py
+++ core/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'common.apps.CommonConfig',
     'account.apps.AccountConfig',
     'blog.apps.BlogConfig',
     'tourplan.apps.TourplanConfig',
