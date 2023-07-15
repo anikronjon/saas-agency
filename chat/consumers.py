9f8290baa454228d69a7bb65b8518f8633dca0b9
anikronjon <anikronjon.swe@gmail.com>
2023-07-15 22:57:25 +0600

    update chat system

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,12 +1,26 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
-
+import json
 
 class MyWebSocket(AsyncWebsocketConsumer):
     async def connect(self):
+        self.group_name = self.scope['url_route']['kwargs']['group_name']
+        self.channel_layer.group_add(self.group_name, self.channel_name)
         await self.accept()
 
     async def disconnect(self, code):
-        await self.close()
+        await self.channel_layer.group_discard(self.group_name, self.channel_name)
 
     async def receive(self, text_data=None, bytes_data=None):
-        await self.send(text_data)
+        data = json.loads(text_data)
+        message = data.get('message', '')
+        await self.channel_layer.group_send(
+            self.group_name, {
+                'type': 'group.message',
+                'message': message
+            }
+        )
+
+    async def chat_message(self, event):
+        await self.send(text_data=json.dumps({
+            'message': event['message']
+        }))
--- chat/routers.py
+++ chat/routers.py
@@ -2,5 +2,5 @@ from django.urls import path, re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    path('ws/', consumers.MyWebSocket.as_asgi())
+    path('ws/<str:group_name>/', consumers.MyWebSocket.as_asgi())
 ]
